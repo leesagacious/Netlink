7e845f43d5b8d94fa298b92b51ce058f5ddfc9d1
leesagacious <sensor1010@163.com>
2017-12-30 19:24:49 +0800

    Update lee_netlink.c

--- lee_netlink.c
+++ lee_netlink.c
@@ -1,1 +1,10 @@
 
+struct leenetlink_area * lee_area;
+
+static const struct proto_ops lee_netlink_ops = {
+    .family  = PF_LEENETLINK,
+    .owner   = THIS_MODULE,
+    .release = leenetlink_relase,
+    .bind    = leenetlink_bind,
+    .connect = leenetlink_connect,
+};
