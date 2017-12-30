3e612402acdef2467f46ee2d9ee015dbe424efb4
leesagacious <sensor1010@163.com>
2017-12-30 19:32:18 +0800

    Update lee_netlink.c

--- lee_netlink.c
+++ lee_netlink.c
@@ -1,10 +1,24 @@
 
-struct leenetlink_area * lee_area;
+struct leenetlink_area * spring_area;
 
-static const struct proto_ops lee_netlink_ops = {
+
+static const struct proto_ops spring_netlink_ops = {
     .family  = PF_LEENETLINK,
     .owner   = THIS_MODULE,
     .release = leenetlink_relase,
     .bind    = leenetlink_bind,
     .connect = leenetlink_connect,
 };
+
+
+static int __init spring_netlinkport_init(void)
+{
+    int ret;
+    
+    spring_area = kcalloc(MAX_LINKS, sizeof *spring_area, GFP_KERNEL);
+    if (!spring_area) 
+        goto alloc_area_fail;
+    
+alloc_area_fail:
+    panic("alloc spring_netlink_area has failure\n");
+}    
