a38c51c496f7f08c1d45e60cc9a2b9cc4f4d0cfc
leesagacious <sensor1010@163.com>
2018-01-22 22:39:43 +0800

    Update leenetlink.h

--- leenetlink.h
+++ leenetlink.h
@@ -1,4 +1,10 @@
 
+enum LEE_NETLINK_ERROR {
+    LEE_NETLINK_SUCCESS = 0,
+    FAMILY_ERROR        = -1,
+    
+};
+
 struct leenetlink_area {
     struct list_head   airing_list;
     unsigned int enroll;
