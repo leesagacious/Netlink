77016642e59423eea8bdb69cec336306ee84ddc7
leesagacious <sensor1010@163.com>
2020-10-13 06:40:52 -0700

    Source code style modification
    Signed-off-by: leesagacious <sensor1010@163.com>

--- lee_netlink.c
+++ lee_netlink.c
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: BSD-2-Clause
+/*
+ * Copyright (c) 2020, li-insects-awaken
+ */
 
 struct leenetlink_area * spring_area;
 
@@ -6,12 +10,8 @@ struct lee_sock {
 	wait_queue_head_t	tar_halt;
 };
 
-/* Protocol handler */
 static const struct net_proto_family spring_netlink_family_ops = {
 	.family = SPRING_NETLINK,
-	/*
-	    The user space socket() function will make the function get called
-	*/		
 	.create = spring_netlink_create,
 };
 
@@ -25,12 +25,12 @@ static int leenetlink_bind(struct socket *sock, struct sockaddr *addr, int addr_
 {
 	int err;
 	unsigned int groups;
-	struct lee_sockaddr_nl *nladdr = (struct sockaddr_nl *)addr; 
-	
+	struct lee_sockaddr_nl *nladdr = (struct sockaddr_nl *)addr;
+
 	if (!nladdr->nl_groups) {
 		err = -EINVAL;
 		goto out;
-	}	
+	}
 
 	groups = spring_area[sk->index].groups;
 	if (groups < 64)
@@ -38,26 +38,24 @@ static int leenetlink_bind(struct socket *sock, struct sockaddr *addr, int addr_
 
 out:
 	return err;
-}	
+}
 
-int netlink_unicast( struct sock * sock, struct sk_buff *skb, unsigned int  portid)
+int netlink_unicast(struct sock *sock, struct sk_buff *skb, unsigned int  portid)
 {
 	int ret;
 	int has_sleep;
 	unsigned long deleaytime;
-        struct sock * otsock;
-	
-	/* timeout */
+	struct sock *otsock;
+
 	delaytime = sock->sk_sndtimeo;
-	
-	/* The bottom does not use hashtable */
+
 	targetsock = via_otsock_adopt_portid(sock, portid);
 	if (!targetsock) {
-	    kfree_skb(skb);
-	    ret = -1;
-	    goto unicast_failed;	
+		kfree_skb(skb);
+		ret = -1;
+		goto unicast_failed;
 	}
-	
+
 	has_sleep = atomic_read(&targetsock->sk_rmem_alloc) - targetsock->sk_rcvbuf;
 	if (has_sleep >= 0) {
 		wait_queue_t taskhalt = {
@@ -65,172 +63,140 @@ int netlink_unicast( struct sock * sock, struct sk_buff *skb, unsigned int  port
 			.func    = default_wait_function,
 			.task_list = {NULL, NULL}
 		};
-		
+
 		set_current_task(TASK_INTERRUPTIBLE);
-		
+
 		add_wait_queue(&leesock->tar_halt, &taskhalt);
 	}
-	
+
 	skb_queue_tail(&targetsock->sk_receive_queue, skb);
-			
+
 	targetsock->sk_data_ready(targetsock);
-	
+
 	return 0;
-	
+
 unicast_failed:
 	return ret;
-}	
+}
 
 struct sock * lee_netlink_kernel_create(unsigned int netlinkindex, struct netlink_kernel_cfg * leecfg)
 {
 	unsigned long groups = 32;
-	
-	if (!spring_area) 
-	    painic("spring_area must be not NULL\n");	
-	
+
+	if (!spring_area)
+	    painic("spring_area must be not null");
+
 	if (netlinkindex < 0 || netlinkindex > 32)
-	    goto kernel_create_failed;
-	
+		goto kernel_create_failed;
+
 	if (leecfg->groups < 32)
-	    groups = 32; 	
-	
+		groups = 32;
+
 	mutex_lock(&spring_area_lock);
-	/*
-		Does not support repeated registration
-	*/
 	if (spring_area[netlinkindex].has_registered) {
 		pr_err("This netlink protocol has been registered\n");
 		goto kernel_create_failed;
 	}
-
 	spring_area[netlinkindex].groups = groups;
-
 	mutex_unlock(&spring_area_lock);
-	
-	
+
+
 kernel_create_failed:
 	return NULL;
-}	
+}
 
 static int is_ple(unsigned int detection_tmp)
-{	
+{
 	do {
-		if (detection_tmp & 1) 
-		    goto checkedout;
-		
+		if (detection_tmp & 1)
+			goto checkedout;
+
 		detection_tmp >>= 1;
-		
-	} while (detection_tmp)
-		
-	return 	LEE_NETLINK_PLE;
-	
+
+	} while (detection_tmp);
+
+	return LEE_NETLINK_PLE;
+
 checkedout:
 	return LEE_NETLINK_SUCCESS;
-}	
+}
 
 static int leenetlink_recvmsg(struct socket * sock, struct msghdr *msg, size_t  len)
 {
-	
-}	
 
-/*
-    func : Copy the buffer data to decide whether it is unicast or multicast
-*/
+}
+
 static int leenetlink_sendmsg(struct socket *sock, struct msghdr *msg, size_t len)
 {
-	int ret;	
+	int ret;
 	unsigned int has_ple;
 	unsigned int has_bound;
 	struct sk_buff *skb;
 	struct sockaddr_nl * addr_lee;
-	
+
 	addr_lee = (struct sockaddr_nl *)msg->msg_name;
-	
+
 	if (addr_lee->nl_family != AF_NETLINK) {
 		ret = FAMILY_ERROR;
 		goto family_error;
-	}	
-	/*
-		1 : not use ffs().
-		2 : Imitation soft interrupt handling
-	*/
+	}
+
 	has_ple = is_ple(addr_lee->nl_groups);
-	
+
 family_error:
 	return ret;
-}	
+}
 
 static const struct proto_ops spring_netlink_ops = {
-    .family  = PF_LEENETLINK,
-    .owner   = THIS_MODULE,
-    .release = leenetlink_relase,
-    .bind    = leenetlink_bind,
-    .connect = leenetlink_connect,
-    .sendmsg = leenetlink_sendmsg,	 
-    .recvmsg = leenetlink_recvmsg,   	 
+	.family  = PF_LEENETLINK,
+	.owner   = THIS_MODULE,
+	.release = leenetlink_relase,
+	.bind    = leenetlink_bind,
+	.connect = leenetlink_connect,
+	.sendmsg = leenetlink_sendmsg,
+	.recvmsg = leenetlink_recvmsg,
 };
 
 static lee_netlink_create(struct net *net, struct socket *sock, int protocol)
 {
-    int err; 
-    /*
-        He is the protagonist
-        It's too important ！
-    */
-    struct sock *sk;
-    struct lee_sock * leesock;	
-    
-    if (!spring_area.has_register)  {
-        err = -ENODEV;
-        goto lee_netlink_create_failure;
-    }       
-    
-    /*
-        user-land api calls the socket API will eventually call the specific agreement of the corresponding function
-        here !
-    */
-    sock->ops = &spring_netlink_ops;
-    
-    sk = sk_alloc(net, PF_LEENETLINK, GFP_KERNEL, &lee_netlink_proto);
-    if (!sk)
-       return -ENOMEM;
-    /* 
-       Use only once, hard-coded here 
-       Very important !
-    */
-    sk->sk_rcvbuf = 32768;	
-	
-    init_waitqueue_head(&leesock->tar_halt);	
-	
-    
-    return 0;
-    
+	int err;
+	struct sock *sk;
+	struct lee_sock * leesock;
+
+	if (!spring_area.has_register)  {
+		err = -ENODEV;
+		goto lee_netlink_create_failure;
+	}
+
+	sock->ops = &spring_netlink_ops;
+
+	sk = sk_alloc(net, PF_LEENETLINK, GFP_KERNEL, &lee_netlink_proto);
+	if (!sk)
+		return -ENOMEM;
+
+	sk->sk_rcvbuf = 32768;
+
+	init_waitqueue_head(&leesock->tar_halt);
+
+	return 0;
+
 lee_netlink_create_failure:
-    return err;
-}  
+	return err;
+}
 
 
 static int __init spring_netlinkport_init(void)
 {
-    int ret;
-    
-    spring_area = kcalloc(MAX_LINKS, sizeof *spring_area, GFP_KERNEL);
-    if (!spring_area) 
-        goto alloc_area_fail;
-    
-    /*
-        func : spring_netlink Agreement registration 
-        
-        PF_LEENETLINK : 27
-        
-        The underlying sock implementation : 
-        -> :
-        rcu_assign_pointer(net_families[PF_LEENETLINK], ops);
-    */
-    sock_register(&spring_netlink_ops);
+	int ret;
+
+	spring_area = kcalloc(MAX_LINKS, sizeof(*spring_area), GFP_KERNEL);
+	if (!spring_area)
+		goto alloc_area_fail;
+
+	sock_register(&spring_netlink_ops);
 
 alloc_area_fail:
-    panic("alloc spring_netlink_area has failure\n");
-}    
+	panic("alloc spring_netlink_area has failure\n");
+}
 
 core_initcall(spring_netlinkport_init);
