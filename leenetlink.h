c70dfc4f95b9fab6778eb8948ed8d65a622d7f2d
leesagacious <sensor1010@163.com>
2020-10-15 04:15:46 -0700

    Source code codestyle mod
    Signed-off-by: leesagacious <sensor1010@163.com>

--- leenetlink.h
+++ leenetlink.h
@@ -1,26 +1,30 @@
+/* SPDX-License-Identifier: BSD-2-Clause */
+/*
+ * Copyright (c) 2020, li-insects-awaken
+ */
 
 enum LEE_NETLINK_ERROR {
-    LEE_NETLINK_SUCCESS = 0,
-    FAMILY_ERROR        = -1,
-    LEE_NETLINK_PLE     = -2,
+	LEE_NETLINK_SUCCESS = 0,
+	FAMILY_ERROR        = -1,
+	LEE_NETLINK_PLE     = -2,
 };
 
 struct spring_netlink_sock {
-    unsigned int flags,
+	unsigned int flags,
 };
 
 struct listener {
-    struct list_head node;
-    unsigned long bitmap;
+	struct list_head node;
+	unsigned long bitmap;
 };
 
 struct leenetlink_area {
-    struct list_head   airing_list;
+	struct list_head   airing_list;
 	struct list_head multi_groups_list;
-    unsigned int enroll;
-    struct listeners *monitor;
-    bool has_registered;
-    unsigned int groups;
+	unsigned int enroll;
+	struct listeners *monitor;
+	bool has_registered;
+	unsigned int groups;
 };
 
 struct lee_netlink_kernel_param {
